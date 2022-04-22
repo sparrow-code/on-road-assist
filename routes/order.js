81a389fdde3fa7a158e40b77de00940b748df433
sparrow-code <64479884+sparrow-code@users.noreply.github.com>
2022-04-22 10:31:40 +0530

    new updates
    New update with the connection of database

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const bodyParser = require('body-parser');
-const { transporter } = require('../mail.js');
+
+
+const { postOrder } = require("../controller/order.js");
+const { postContact } = require("../controller/contact.js");
 
 // parse application/x-www-form-urlencoded
 router.use('/', bodyParser.urlencoded({ extended: false }))
@@ -21,35 +24,6 @@ router.get('/:slug', (req, res) => {
 
 
 router.use('/', bodyParser.json())
-router.post('/', (req, res) => {
-    var req_data = {
-        veh_reg_no: req.body.veh_reg_no,
-        full_name: req.body.full_name,
-        e_mail_id: req.body.e_mail_id,
-        mo_no: req.body.mo_no,
-        area: req.body.area,
-        current_location: req.body.current_location,
-        frnd_no_1: req.body.frnd_no_1,
-        frnd_no_2: req.body.frnd_no_2,
-        frnd_no_3: req.body.frnd_no_3
-    }
-
-    var mailOptions = {
-        from: 'admin@zeroxbit.com',
-        to: req_data.e_mail_id,
-        subject: 'Your Order Place Successfully',
-        text: 'Rate your experience with us',
-    };
-
-    // var mail_data = {};
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        }
-    });
-
-    res.redirect('/sucessful');
-})
+router.post('/', postOrder);
 
 module.exports = router;
