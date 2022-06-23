e71a8c9c45b70740bb75193b594bd7dee9417030
sparrow-code <64479884+sparrow-code@users.noreply.github.com>
2022-06-24 00:24:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var title = process.env.TITLE;
 // Require Module
 var path = require('path')
 var express = require('express');
-
+const cookieParser = require("cookie-parser");
+const session = require('express-session');
 
 var { engine } = require('express-handlebars')
 
@@ -18,16 +19,27 @@ app.set('view engine', 'handlebars');
 
 // Expose This directory
 app.use(express.static(path.join(__dirname, './assets/')));
+app.use(cookieParser());
 
+// Create Cookie
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+app.use(session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay, secure : false },
+}));
+
+// var session;
 
 // Router Imports
 var order = require('./routes/order.js')
 var send_msg = require('./routes/send_msg.js');
-
-// Routes Use
-app.use('/order', order);
-app.use('/send_msg', send_msg);
-
+var dashboard = require('./routes/dashboard.js');
+var {postLogin} = require('./controller/login.js');
 
 // Api Router
 var apiOrder = require("./api/order.js");
@@ -37,6 +49,11 @@ var apiSendMsg = require("./api/snd_msg.js");
 app.use("/api/order", apiOrder );
 app.use("/api/sendMsg", apiSendMsg);
 
+
+// Routes Use
+app.use('/order', order);
+app.use('/send_msg', send_msg);
+app.use('/dashboard', dashboard);
 
 app.get('/', (req, res) => {
     var send_data = {
@@ -83,9 +100,28 @@ app.get('/register', (req, res) => {
     res.render('register', {});
 })
 
+
+app.use('/login', express.json());
+app.use('/login', express.urlencoded({ extended: true }));
+// cookie parser middleware
+
 app.get('/login', (req, res) => {
-    res.render('login', {});
+
+    if(req.session.officeID) {
+        res.redirect('/dashboard');
+    } else {
+        const send_data = {
+            title : "Login - On Road Assist",
+            // home : false,
+            login : false
+        };
+
+        res.render('login', {data : send_data} );
+    }
 })
+
+// var session ;
+app.post('/login', postLogin);
 
 app.get('/sucessful', (req, res) => {
     res.render('sucessful', {});
